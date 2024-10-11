bbc6752599ee478a06121470bb829e840584e349
Mashrur Chowdhury <mashrurchowdhury@bitgo.com>
2024-10-11 15:48:46 -0400

    fix login redirect

--- app/src/main/java/com/example/tripwise/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/tripwise/ui/navigation/AppNavigation.kt
@@ -1,6 +1,8 @@
 package com.example.tripwise.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -10,17 +12,18 @@ import com.example.tripwise.ui.screens.DashboardScreen
 import com.example.tripwise.ui.screens.AddEditScreen
 
 @Composable
-fun AppNavigation(navController: NavHostController, onGoogleSignInClicked: () -> Unit, modifier: Modifier = Modifier) {
+fun AppNavigation(
+    navController: NavHostController,
+    onGoogleSignInClicked: () -> Unit,
+    shouldNavigateToDashboard: MutableState<Boolean>,
+    modifier: Modifier = Modifier
+) {
     NavHost(navController, startDestination = "login") {
         composable("login") {
             LoginScreen(
                 modifier = modifier,
                 onGoogleSignInClicked = {
                     onGoogleSignInClicked()
-                },
-                onLoginClicked = {
-                    // Navigate to the next screen
-                    navController.navigate("dashboard")
                 }
             )
         }
@@ -29,8 +32,8 @@ fun AppNavigation(navController: NavHostController, onGoogleSignInClicked: () ->
                 // Navigate to the add screen
                 navController.navigate("add-edit")
             }, onEditClick = {
-            // Navigate to the add screen
-            navController.navigate("add-edit")
+                // Navigate to the add screen
+                navController.navigate("add-edit")
             })
         }
         composable("add-edit") {
@@ -38,6 +41,16 @@ fun AppNavigation(navController: NavHostController, onGoogleSignInClicked: () ->
                 // Go to previous screen
                 navController.navigateUp()
             })
+        }
+    }
+
+    // Navigate to dashboard if the state is true
+    LaunchedEffect(shouldNavigateToDashboard.value) {
+        if (shouldNavigateToDashboard.value) {
+            navController.navigate("dashboard") {
+                popUpTo("login") { inclusive = true }
+            }
+            shouldNavigateToDashboard.value = false
         }
     }
 }
