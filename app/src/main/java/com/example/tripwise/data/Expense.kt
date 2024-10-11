fbd0a0027c3adab9b6d465acb4d98833eae09bac
kevinzhang30 <kevinzhang2115@gmail.com>
2024-10-10 22:59:52 -0400

    fixed something

--- app/src/main/java/com/example/tripwise/data/Expense.kt
+++ app/src/main/java/com/example/tripwise/data/Expense.kt
@@ -1,4 +1,12 @@
 package com.example.tripwise.data
 
-class Expense {
-}
+data class Expense(
+    val expenseId: Long,
+    val tripId: Long,
+    val name: String,
+    val cost: Double,
+    val currency: String,
+    val date: String,
+)
+
+
--- app/src/main/java/com/example/tripwise/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/tripwise/ui/navigation/AppNavigation.kt
@@ -7,6 +7,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.tripwise.ui.screens.LoginScreen
 import com.example.tripwise.ui.screens.DashboardScreen
+import com.example.tripwise.ui.screens.TripDetailScreen
 
 @Composable
 fun AppNavigation(modifier: Modifier = Modifier) {
@@ -23,7 +24,15 @@ fun AppNavigation(modifier: Modifier = Modifier) {
             )
         }
         composable("dashboard") {
-            DashboardScreen(modifier = modifier)
+            DashboardScreen(
+                modifier = modifier
+            )
+        }
+        composable(route = "trip") {
+            TripDetailScreen(
+                modifier = modifier,
+                onBackClick = { navController.popBackStack() } // Use popBackStack for back navigation
+            )
         }
     }
 }
