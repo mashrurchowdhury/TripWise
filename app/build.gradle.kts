7fd3342dcc2ad0eaaa2983a789a52a5eabeedbfc
Mashrur Chowdhury <mashrurchowdhury@bitgo.com>
2024-12-02 00:24:16 -0500

    v0.40 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "0.30"
+        versionName = "0.40"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         javaCompileOptions {
